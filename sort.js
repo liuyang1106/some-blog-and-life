480a3c1c5b7be7e67dc131e73acff41645fce824
leoliu <1106841116@qq.com>
2019-09-26 17:14:05 +0800

    修改了冒泡排序的bu gbug

--- sort.js
+++ sort.js
@@ -1,36 +1,43 @@
 // 冒泡排序，主要是交换，得到最后排序的数组
-// 每轮挑选出最大的
+// 原理很简单，就是相邻的两个元素交换，把最大的那个浮出水面，就撑之为冒泡
 function bubbleSort(arr) {
+  console.time('bubbleSort：');
   if (arr.length <= 1) return arr;
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[i] > arr[j]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
+  console.timeEnd('bubbleSort：');
   return arr;
 }
+//结论
+bubbleSort([1, 2, 4, 5, 6, 78, 9, 21, 4, 1, 2]);
 // 优化自己的冒泡排序
 // 算法原理，1.前后同时遍历，找到最大的最小的，并且下次排序去掉头尾
 function betterBubbleSort(arr) {
+  console.time('betterBubbleSort');
   let low = 0,
     high = arr.length - 1;
   while (low < high) {
+    // 只找出最小的那个
     for (let i = low; i < high; i++) {
-      // 找打最小的放在最前面
-      if (arr[i] < arr[low]) {
-        [arr[i], arr[low]] = [arr[low], arr[i]];
-        low++;
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
       }
     }
-    for (let i = high; i >= low; i--) {
-      //找到最大的放最后面
-      if (arr[i] > arr[high]) {
-        high--;
-        [arr[i], arr[high]] = [arr[high], arr[i]];
+    for (let i = high; i > low; i--) {
+      if (arr[i] < arr[i - 1]) {
+        [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
       }
     }
+    low++;
+    high--;
   }
+  console.timeEnd('betterBubbleSort');
   return arr;
 }
+// 结论
+betterBubbleSort([1, 2, 4, 5, 6, 78, 9, 21, 4, 1, 2]);
